0b4f947e0f798e7769ec5c2113205736aa94d1f9
Tyson Frederick <tkinetik1@gmail.com>
2019-05-09 12:42:44 -0700

    Add button ids in activity_quiz.xml; create member variable button instances for true and false buttons. yet to be implemented in onCreate.

--- app/src/main/java/com/tysonfrederick/android/digitalmediasoftwarequiz/QuizActivity.java
+++ app/src/main/java/com/tysonfrederick/android/digitalmediasoftwarequiz/QuizActivity.java
@@ -2,7 +2,9 @@ package com.tysonfrederick.android.digitalmediasoftwarequiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -15,6 +17,20 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+           // Implemented - Does nothing yet, but soon!! - TF 2019
+               Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+           }
+        });
         mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // implemented new Toast - tf - will be using commits from now out to comment when possible
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
